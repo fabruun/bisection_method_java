af263cf77dbb0ae8584c731178c2f9d77e20494d
Frederik Bruun <frederikabruun@gmail.com>
2020-09-27 18:38:18 +0200

    bisection class

---
+++ Bisection.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Bisection {
+    
+    public static  void main(String[] args) {
+        System.out.println(bisection(-5, 5, 0.0000001));
+    }
+
+    private static double f(double x) {
+        return (2*x*x) + (20*x) + 42;
+    }
+
+    private static double bisection(double a, double b, double error) {
+        double c = 0;
+
+        while(a - b < error) {
+            c = (a + b) / 2;
+            if(f(c) == 0) {
+                break;
+            }
+            if(f(a) * f(c) < 0) {
+                b = c;
+            } else {
+                a = c;
+            }
+        }
+
+        return c;
+    }
+
+}
